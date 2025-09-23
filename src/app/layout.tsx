1ed6855ec36dcf477533484395a61d99bbb65759
devnoman91 <dev.nali91@gmail.com>
2025-09-24 02:05:19 +0500

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,7 @@ export default async function Home() {
       )}
      
       
-      <Footer/>
+   
     </div>
   );
 }

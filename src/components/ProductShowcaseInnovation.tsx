ce8949fe34c4917a35217757606f612fe11f9e97
devnoman91 <dev.nali91@gmail.com>
2025-11-13 00:13:08 +0500

    add links

--- src/components/ProductShowcaseInnovation.tsx
+++ src/components/ProductShowcaseInnovation.tsx
@@ -6,6 +6,8 @@ import { motion, PanInfo } from 'framer-motion'
 interface BlogItem {
   title: string
   description: string
+  buttonText?: string
+  buttonLink?: string
   slug: { current: string }
 }
 
@@ -192,11 +194,11 @@ const BlogCard: React.FC<{ blog: BlogItem; index: number }> = React.memo(({ blog
       className="relative overflow-hidden cursor-pointer px-[24px] py-[5px] rounded-[50px] border  border-white text-white font-helvetica text-[14px] leading-[24px] font-medium w-fit block mt-auto group transition-colors duration-500 hover:border-[#560100]"
       whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.98 }}
-      onClick={() => (window.location.href = `/blog/${blog.slug.current}`)}
+      onClick={() => (window.location.href = blog.buttonLink || `/blog/${blog.slug.current}`)}
     >
       <span className="absolute inset-0 bg-[#560100] translate-x-full transition-transform duration-500 rounded-[50px] group-hover:translate-x-0" />
       <span className="relative z-10 transition-colors duration-500 group-hover:text-white">
-        Learn More
+        {blog.buttonText || 'Learn More'}
       </span>
     </motion.button>
   </motion.div>
